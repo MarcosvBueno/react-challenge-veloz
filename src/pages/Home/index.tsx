bec39a76abdaac8f371914f2179c2dc224c76d65
MarcosvBueno <mvbueno07@gmail.com>
2023-11-23 20:25:19 -0300

    chore(main): adding packages and structuring folders

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import { Text } from "react-native";
+
+function HomePage() {
+    return (
+    <>
+            <Text>Home Page</Text>
+            
+
+    </>
+    );
+}
+
+export default HomePage;
